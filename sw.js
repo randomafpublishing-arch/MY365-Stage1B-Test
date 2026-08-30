f49bc8a104087608285aa0d5375497168d41f4ea
randomafpublishing-arch <randomafpublishing@gmail.com>
2026-08-29 15:32:24 -1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'my365-stage2-test-v1';
+
+const ASSETS = [
+  './index.html',
+  './themes/hawaiian/01_iwa.png',
+  './themes/dog/01_golden_retriever.png',
+  './themes/zodiac-celestial/01_Aries.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME && key.startsWith('my365-stage2-test-'))
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request);
+    })
+  );
+});
